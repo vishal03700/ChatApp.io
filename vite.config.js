aee2cdcfed94e35c620932b32f3538696c8d3746
VISHAL KUMAR YADAV <vishal@VISHALs-MacBook-Air-2.local>
2025-06-25 19:22:57 +0530

    Updation5

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://chatapp-io-2.onrender.com/', // or your local backend URL
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
